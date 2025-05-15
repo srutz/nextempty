70af5d1ee8c40fb7a5e92588f003d29b2a5b5be0
stepan.rutz <stepan.rutz@gmx.de>
2025-05-15 09:38:33 +0200

    use context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Counter } from "@/components/Counter";
+import { ApplicationInfos } from "@/components/ApplicationInfos";
 
 export default function Home() {
     return (
         <div className="bg-orange-300 grow flex flex-col">
-            <Counter></Counter>
+            <div>Home page</div>
+            <ApplicationInfos></ApplicationInfos>
         </div>
     )
 }
