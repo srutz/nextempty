b140e2b67076a3ea47d1e97f8ed4f2f290d8ee29
stepan.rutz <stepan.rutz@gmx.de>
2025-05-15 22:19:43 +0200

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Counter } from "@/components/Counter";
 
 export default function Home() {
     return (
-        <div className="bg-orange-300 grow flex flex-col">
+        <div className="bg-orange-300 grow flex flex-col items-center">
             <Counter></Counter>
         </div>
     )
