7eef889ffedcd674a828538ea7ab9df50dcca14b
stepan.rutz <stepan.rutz@gmx.de>
2025-05-15 18:08:49 +0200

    fixed usestatementmanagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import { Sidebar } from "@/components/Sidebar";
-import { useGuiStore } from "@/hooks/GuiStateStore";
+import { ToggleSidebar } from "@/components/ToggleSidebar";
 
 export default function Home() {
-    const { showSidebar, setShowSidebar } = useGuiStore()
     return (
         <div className="grow flex flex-col">
             <div className="h-1 grow flex">
                 <Sidebar></Sidebar>
                 <div className="grow self-stretch bg-slate-200 flex flex-col gap-2 items-center justify-center">
                     <div>Home page</div>
-                    <button onClick={() => {}}>Toggle Sidebar</button>
+                    <ToggleSidebar></ToggleSidebar>
                 </div>
             </div>
         </div>
